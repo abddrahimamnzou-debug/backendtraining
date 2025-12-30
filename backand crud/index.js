612bdc1473798792c04314f2409916f825820c62
abddrahimamnzou-debug <abddrahimamnzou@gmail.com>
2025-12-30 12:01:02 +0100

    ozdo

--- backand crud/index.js
+++ backand crud/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('✔ MongoDB connected'))
+    .then(() => console.log('✔ MongoDB connected done'))
     .catch(err => console.error('Connection error:', err));
 
 // Routes
